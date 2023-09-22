b0aaa3c25d582dd538e37bdffa6f1b2f11165c80
ErnestoCarrizo <67176861+ErnestoCarrizo@users.noreply.github.com>
2023-09-21 23:15:33 -0300

    CrearPartida

--- src/entities.py
+++ src/entities.py
@@ -3,7 +3,7 @@ from enumerations import (Kind, Role, CardName)
 
 db = Database()
 
-class Card(db.Entity):
+""" class Card(db.Entity):
     id = PrimaryKey(int, auto=True)
     name = Required(CardName, default=CardName.THE_THING)
     description = Required(str, default="description")
@@ -11,35 +11,48 @@ class Card(db.Entity):
     kind = Required(Kind, default=Kind.ACTION)
     game = Required('Game', reverse="deck")
     player = Required('Player', reverse="hand")
-    required_players = Required(int, default=0)
+    required_players = Required(int, default=0) """
 
-class Player(db.Entity):
+class User(db.Entity):
+    """An User that can or nor host a game"""
     id = PrimaryKey(int, auto=True)
     name = Required(str)
-    host = Optional("Game", reverse="host")
-    game = Required('Game', reverse="players")
+    #player = Optional("Player")
+    game = Optional("Game")
+
+class Game(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    password = Optional(str)
+    host = Required(User)
+    
+""" class Player(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    user = Required("User")
+    #game = Required('Game', reverse="players")
     position = Required(int, default=0)
     role = Required(Role, default=Role.HUMAN)
     is_dead = Required(bool, default=False)
     hand = Set(Card)
     in_lockdown = Required(bool, default=False)
     left_barrier = Required(bool, default=False)
-    right_barrier = Required(bool, default=False)
+    right_barrier = Required(bool, default=False) """
 
-class Game(db.Entity):
+""" class Game(db.Entity):
     id = PrimaryKey(int, auto=True) 
     name = Required(str)
-    host = Required(Player, reverse="host")
-    players = Set(Player)
+    host = Required(User)
+    # players = Set(Player)
     current_turn = Required(int, default=0)
     in_game = Required(bool, default=False)
     is_done = Required(bool, default=False)
     password = Optional(str)
     going_clockwise = Required(bool, default=True)
-    min_players = Required(int)
-    max_players = Required(int)
+    min_players = Required(int, default=4)
+    max_players = Required(int, default=12)
     deck = Set(Card)
-    number_of_players = Required(int, default=0)
+    number_of_players = Required(int, default=0) """
 
 
 db.bind('sqlite', 'example.sqlite', create_db=True)
--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pony.orm import db_session
-from entities import (Card, Player, Game, db)
+from entities import (User, Game, db)
 from schemas import CreateGameIn
 
 app = FastAPI()
@@ -12,5 +12,8 @@ async def root():
 @app.post("/game")
 async def create_game(form: CreateGameIn) -> CreateGameIn:
     with db_session:
-        game = Game(name=form.game_name, host=Player(name=form.player_name), min_players=form.min_players, max_players=form.max_players, password=form.password)
+        host_user = User(name=form.player_name)
+        game = Game(name="test", password="1234", host=host_user)
+        """ player = Player(name=form.player_name, user=host_user)
+        game = Game(name=form.game_name, host=host_user, min_players=form.min_players, max_players=form.max_players, password=form.password) """
     return form
