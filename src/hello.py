5720726df89e086002da7a2a0992e193b6b06fd9
ErnestoCarrizo <67176861+ErnestoCarrizo@users.noreply.github.com>
2023-09-21 22:15:56 -0300

    Defaults and tests

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pony.orm import db_session
 from entities import (Card, Player, Game, db)
 from schemas import CreateGameIn
 
@@ -10,4 +11,6 @@ async def root():
 
 @app.post("/game")
 async def create_game(form: CreateGameIn) -> CreateGameIn:
+    with db_session:
+        game = Game(name=form.game_name, host=Player(name=form.player_name), min_players=form.min_players, max_players=form.max_players, password=form.password)
     return form
