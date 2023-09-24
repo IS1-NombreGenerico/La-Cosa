a0f9137f4ec7342bc92e94ac1c295b5840e7e123
Juli Storino <julieta_storino@mi.unc.edu.ar>
2023-09-24 16:09:27 -0300

    Join player update

--- src/hello.py
+++ src/hello.py
@@ -5,7 +5,6 @@ from entities import Player, Game
 from schemas import CreateGameIn, CreateGameResponse, GameOut, PlayerIn, PlayerResponse, PlayerOut
 from utils import db_game_2_game_out, db_player_2_player_out
 
-
 app = FastAPI()
 
 @app.get("/")
@@ -30,44 +29,84 @@ async def list_availables_games() -> List[GameOut]:
                 if filter_by_availability(g)
             )
         ]
+
     return games
 
-@app.post("/join")
-async def join_game(game_info: GameOut, player_info: PlayerIn) -> PlayerResponse:
+@app.post("/join/{id_game}")
+async def join_game(id_game: int, player_info: PlayerIn) -> PlayerResponse:
+
     if not player_info.player_name:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="EMPTY_NAME",
         )
-    
-    if password and game_info.password != player_info.password:
-        raise HTTPException(
-            status_code=status.HTTP_407_INVALID_PASSWORD,
-            detail="INVALID_PASSWORD",
-        )
 
     with db_session:
+        
+        db_game = select(g for g in Game if g.id == id).first()
+        
+        if not db_game:
+            raise HTTPException(
+                status_code=status.HTTP_408_INVALID_GAME,
+                detail="INVALID_GAME",
+            )
+        if db_game.in_game:
+            raise HTTPException(
+                status_code=status.HTTP_409_GAME_IN_PROGRESS,
+                detail="GAME_IN_PROGRESS",
+            )
+        if db_game.number_of_players >= db_game.max_players:
+            raise HTTPException(
+                status_code=status.HTTP_403_COMPLETE_QUOTE,
+                detail="COMPLETE_QUOTE",
+            )
+        if db_game.password and db_game.password != player_info.password:
+            raise HTTPException(
+                status_code=status.HTTP_407_INVALID_PASSWORD,
+                detail="INVALID_PASSWORD",
+            )
+
         p = Player(
             name = player_info.player_name,
-            game = game_info.id,
-            position = game_info.number_of_players,
+            game = db_game,
+            position = db_game.number_of_players,
             role = HUMAN,
             is_dead = False,
             in_lockdown = False,
             left_barrier = False,
             right_barrier = False,
         )
-        return PlayerResponse(id=p.id)
 
-@app.delete("/{id_player}/{id_game}")
-async def leave_game(id_player: int, id_game: int) -> PlayerResponse:
+        db_game.number_of_players += 1
+        db_game.players.add(p)
+
+    return PlayerResponse(id=p.id)
+
+@app.delete("/{id_game}/{id_player}")
+async def leave_game(id_game: int, id_player: int) -> PlayerResponse:
 
     with db_session:
+    
         db_player = select(p for p in Player if p.id == id).first()
+        db_game = select(g for g in Game if g.id == id).first()
+
         if not db_player:
             raise HTTPException(
                 status_code=status.HTTP_406_INVALID_PLAYER,
                 detail="INVALID_PLAYER",
             )
+        if not db_game:
+            raise HTTPException(
+                status_code=status.HTTP_408_INVALID_GAME,
+                detail="INVALID_GAME",
+            )
+        if db_game.host == db_player:
+            raise HTTPException(
+                status_code=status.HTTP_401_IS_HOST,
+                detail="IS_HOST",
+            )
+
         db_player.delete()
-        return PlayerResponse(id=id)
+        db_game.number_of_players -= 1
+
+    return PlayerResponse(id=id)
